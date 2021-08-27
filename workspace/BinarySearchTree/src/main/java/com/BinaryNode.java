fdc5d0ef32340f1cf79863f24c18af42507ece94
Jay22031999 <Jay22031999>
2021-08-27 13:57:08 +0530

    welcome message

---
+++ workspace/BinarySearchTree/src/main/java/com/BinaryNode.java
@@ -0,0 +1,16 @@
+package main.java.com;
+
+public class BinaryNode<K extends Comparable<K>> {
+		
+	K key;
+	BinaryNode<K> left;
+	BinaryNode<K> right;
+		
+		
+	public BinaryNode(K key){
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+}
+
